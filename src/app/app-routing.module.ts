a4b3db9bf4bb3529fc968343acae783afada2450
doumova <doumogueye.mg@gmail.com>
2022-06-28 01:44:33 +0100

    add questions and displaying user name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionComponent } from './question/question.component';
+import { WelecomeComponent } from './welecome/welecome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'welecome', pathMatch:"full"},
+  {path: "", component: WelecomeComponent},
+  {path: "question", component: QuestionComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelecomeComponent } from './welecome/welecome.component';
+import { QuestionComponent } from './question/question.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelecomeComponent,
+    QuestionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
